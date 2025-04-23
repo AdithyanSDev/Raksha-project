9638a66c3df8e722fa6974618c6ed57c594bce60
AdithyanSDev <adithyanshajilal@gmail.com>
2025-04-23 15:44:21 +0530

    Fixed the app.ts for production

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -35,14 +35,16 @@ app.use(
 );
 
 const currentDir = path.resolve();
-const frontendPath = path.join(currentDir, "../Frontend/dist");
 
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(currentDir, "../Frontend/dist");
+  app.use(express.static(frontendPath));
 
-app.use(express.static(frontendPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 // Initialize session middleware
 app.use(
